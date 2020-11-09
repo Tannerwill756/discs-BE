4ca09a03172773755943d2328335fde7e7eb2a38
Tanner Williams <tannerwill756@gmail.com>
2020-11-09 14:28:55 -0500

    setting up db server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const server = express();
-
+const mongoose = require('mongoose');
 
 server.use(express.json());
+
+mongoose.connect('mongodb://localhost/discgolf', { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log("Connected to Database"))
 
 server.get("/", (req, res) => {
     res.json({ api: "up" });
